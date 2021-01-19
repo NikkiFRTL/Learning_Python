8310103fd6b1a89c834c2f8a7b1180fe5526e39b
NikkiFRTL <70467189+NikkiFRTL@users.noreply.github.com>
2021-01-20 00:17:59 +0500

    Commit #9

---
+++ 10.13 User Check.py
@@ -0,0 +1,42 @@
+import json
+
+
+def get_stored_user():
+    """
+    Gets a stored user's name whether exists
+    """
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            user = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return user
+
+
+def get_new_user():
+    """
+    Requests a user's name
+    """
+    user = input('What is your name??\n')
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(user, f)
+    return user
+
+
+def greet_user():
+    """
+    Greets a user
+    """
+    user = get_stored_user()
+    current_user = input(f'Hello! Are you {user}? yes/no\n')
+    if current_user.lower() == 'yes':
+        print(f'Welcome back, {user}!')
+    else:
+        user = get_new_user()
+        print(f"We'll remember you when you come back, {user}.")
+
+
+greet_user()
